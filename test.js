5134dc3f0f1e0944dfb38827d2e78cf15da00cf5
Rafael Cortês <mrfyda@gmail.com>
2015-01-27 16:18:39 +0000

    Update test.js

--- test.js
+++ test.js
@@ -39,9 +39,21 @@ a();
 b();
 
 var outrascenas = '123asd' + "321";
-var as= "321";
+var as= "321";var cen2as = "asd132";
+
+var cemast = '123asd';
+a();
 
 
+var censad_as = "asd132";
+
+var cemsadasdfsaFSAP_as = '123asd';
+a();
+
+var cendsadasdasDsaas = "asd132";
+
+var cemas = '123asd';
+a();
  ; var hr = '123asd' + "321";
 
 ;;;;;;
