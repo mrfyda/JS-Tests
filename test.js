0eb4f50e1719a1c6cb241c9cccf15dc33f509789
Rafael Cortês <mrfyda@gmail.com>
2015-01-27 16:16:23 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,47 @@
+function a() {
+  console.log("asd" + 'dsa');
+}
 
+
+
+if(1 == "2" && "3" == 5) {
+  return;
+}
+
+var cenas = "asd132";
+
+var cemas = '123asd';
+a();
+
+b();
+
+var outrascenas = '123asd' + "321";
+var as= "321";
+
+
+ ; var hr = '123asd' + "321";
+
+;;;;;;function a() {
+  console.log("asd" + 'dsa');
+}
+
+
+
+if(1 == "2" && "3" == 5) {
+  return;
+}
+
+var cenas = "asd132";
+
+var cemas = '123asd';
+a();
+
+b();
+
+var outrascenas = '123asd' + "321";
+var as= "321";
+
+
+ ; var hr = '123asd' + "321";
+
+;;;;;;
