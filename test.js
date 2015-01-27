7f47782caec67da19bc4b053e42f596de97fdb10
Rafael Cortês <mrfyda@gmail.com>
2015-01-27 17:17:49 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,1 @@
 
-function a() {
-  console.log("asd" + 'dsa');
-}
-
-
-
-if(1 === 2 && 3 === 5) {
-  return;
-}
-
-
-if(true) return;
-
-if(2 == 1) return
-
-if(2 == 1) return
-
-if(2 == 1) return
